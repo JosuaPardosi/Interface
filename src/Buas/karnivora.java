c97d89689db5ded9718b4406246aaef56461578c
JosuaPardosi <josua1811@gmail.com>
2021-07-13 13:54:40 +0700

    Test

---
+++ src/Buas/karnivora.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Buas;
+
+import pkginterface.Hewan;
+
+/**
+ *
+ * @author Kenji
+ */
+public class karnivora extends Hewan{
+    
+}
